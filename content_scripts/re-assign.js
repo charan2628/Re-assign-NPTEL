9287828c391cbcffdbba5946289990b95c8eb82c
schar <s.charancherry22@gmail.com>
2021-04-03 08:32:41 +0530

    Done UI and content scripts

---
+++ content_scripts/re-assign.js
@@ -0,0 +1,57 @@
+(function() {
+
+    if (window.hasRunReAssignNptel) {
+        return;
+    }
+    window.hasRunReAssignNptel = true;
+
+    /*
+    * Clears answer and add as show answer button for each quesion.
+    */
+    function hideAnswers() {
+        // clear and enable input
+        Array.prototype.forEach.call(document.querySelectorAll(".qt-choices .gcb-mcq-choice input"), function (radio) {
+            radio.disabled = false; radio.checked = false;
+        });
+        // clear answers
+        Array.prototype.forEach.call(document.querySelectorAll(".qt-feedback"),  (elem) => {
+            elem.style.display = "none";
+
+            // create button for first button for one time and reuse it 
+            if (window.hasRunReAssignNptel) {
+                Array.prototype.forEach.call(document.querySelectorAll(".re_assign_nptel_show_answer_button"), function (elem) {
+                    elem.style.display = "";
+                }); 
+            } else {
+                // show answer button which displays answer onclick
+                const showAnswerButton = document.createElement("button");
+                showAnswerButton.classList.add("gcb-button", "re_assign_nptel_show_answer_button");
+                showAnswerButton.textContent = "SHOW ANSWER";
+                showAnswerButton.onclick = function() {
+                    elem.style.display = "";
+                }
+        
+                elem.parentNode.insertBefore(showAnswerButton, elem);
+            }
+        });
+    }
+
+    // Unhides the answers for all questions
+    function showAnswers() {
+        Array.prototype.forEach.call(document.querySelectorAll(".qt-feedback"), function (elem) {
+            elem.style.display = "";
+        }); 
+        Array.prototype.forEach.call(document.querySelectorAll(".re_assign_nptel_show_answer_button"), function (elem) {
+            elem.style.display = "none";
+        }); 
+    }
+
+    // adding Listeners to hide and show answers
+    browser.runtime.onMessage.addListener((message) => {
+        if (message.command === "re_assign_nptel_hideAnswers") {
+            hideAnswers();
+        } else if (message.command === "re_assign_nptel_showAnswers") {
+            showAnswers();
+        }
+    });
+})();
---
+++ popup/index.js
@@ -0,0 +1,32 @@
+const toggleSwitch = document.getElementById("toggle");
+const hideAnswers = document.getElementById("hide");
+const showAnswers = document.getElementById("show");
+
+let contentScriptRegistered = null;
+let testValue = 0;
+
+//toggle switch to load content scripts
+toggleSwitch.addEventListener('input', (elem) => {
+    if (elem.target.checked) {
+        browser.tabs.executeScript({
+            file: "/content_scripts/re-assign.js"
+        });
+    }
+});
+
+//send hideAnswers message which will be picked up content script
+hideAnswers.addEventListener("click", () => {
+    browser.tabs.query({active: true, currentWindow: true})
+            .then((tabs) => {
+                browser.tabs.sendMessage(tabs[0].id, {command: "re_assign_nptel_hideAnswers"});
+            });
+});
+
+//send showAnswers message which will be picked up content script
+showAnswers.addEventListener("click", () => {
+    console.log('show clicked');
+    browser.tabs.query({active: true, currentWindow: true})
+            .then((tabs) => {
+                browser.tabs.sendMessage(tabs[0].id, {command: "re_assign_nptel_showAnswers"});
+            });
+});
